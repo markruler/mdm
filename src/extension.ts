121228c75bd715e8305d7084195ae19520469786
Changsu Im <imcxsu@gmail.com>
2023-02-20 22:56:19 +0900

    Fix indentation

--- src/extension.ts
+++ src/extension.ts
@@ -1,43 +1,43 @@
 import * as vscode from "vscode";
 import * as MarkdownIt from "markdown-it";
-import {callMailer, getMailServer} from "./mailer";
+import { getMailServer, callMailer } from "./mailer";
 
 // ex: https://github.com/microsoft/vscode-extension-samples/blob/main/github-authentication-sample/src/extension.ts
 export async function activate(context: vscode.ExtensionContext) {
-    console.log("activate");
+  console.log("activate");
 
-    const md: MarkdownIt = new MarkdownIt();
-    const mailServer: string = await getMailServer();
-    const mailHostPort = mailServer.split(":");
+  const md: MarkdownIt = new MarkdownIt();
+  const mailServer: string = await getMailServer();
+  const mailHostPort = mailServer.split(":");
 
-    let send = vscode.commands.registerCommand("mdm.send", async () => {
-        if (!vscode.window.activeTextEditor) {
-            return; // no editor
-        }
+  let send = vscode.commands.registerCommand("mdm.send", async () => {
+    if (!vscode.window.activeTextEditor) {
+      return; // no editor
+    }
 
-        const {document} = vscode.window.activeTextEditor;
+    const { document } = vscode.window.activeTextEditor;
 
-        const scheme = document.uri.scheme;
-        if (!scheme || scheme !== "file") {
-            return; // not a file
-        }
-        console.log(document.uri);
+    const scheme = document.uri.scheme;
+    if (!scheme || scheme !== "file") {
+      return; // not a file
+    }
+    console.log(document.uri);
 
-        const activeFilePath = document.uri.path;
-        const message = `# current file ${activeFilePath}`;
+    const activeFilePath = document.uri.path;
+    const message = `# current file ${activeFilePath}`;
 
-        const html = md.render(message);
-        console.log(html);
+    const html = md.render(message);
+    console.log(html);
 
-        console.log(`mailServer: ${mailServer}`);
-        await callMailer(html, mailHostPort[0], parseInt(mailHostPort[1]));
+    console.log(`mailServer: ${mailServer}`);
+    await callMailer(html, mailHostPort[0], parseInt(mailHostPort[1]));
 
-        vscode.window.showInformationMessage(`Send to ${mailServer}`);
-    });
+    vscode.window.showInformationMessage(`Send to ${mailServer}`);
+  });
 
-    context.subscriptions.push(send);
+  context.subscriptions.push(send);
 }
 
 export function deactivate() {
-    console.log("deactivate");
+  console.log("deactivate");
 }
--- src/mailer.ts
+++ src/mailer.ts
@@ -2,42 +2,43 @@ import * as vscode from "vscode";
 import * as nodemailer from "nodemailer";
 
 let mailOptions = {
-    from: "imcxsu@gmail.com",
-    to: "imcxsu@gmail.com",
-    subject: "Hello ✔",
-    html: "",
+  from: "imcxsu@gmail.com",
+  to: "imcxsu@gmail.com",
+  subject: "Hello ✔",
+  html: "",
 };
 
 export async function callMailer(html: string, host: string, port: number) {
-    let transporter = nodemailer.createTransport({
-        host: host,
-        port: port,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            // user: "imcxsu@gmail.com",
-            // pass: account.pass
-        },
-    });
+  let transporter = nodemailer.createTransport({
+    host: host,
+    port: port,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      // user: "imcxsu@gmail.com",
+      // pass: account.pass
+    },
+  });
 
-    mailOptions.html = html;
+  mailOptions.html = html;
 
-    await transporter.sendMail(mailOptions).then((info) => {
-        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-        console.log("Message sent: %s", info.response);
-    });
+  await transporter.sendMail(mailOptions).then((info) => {
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    console.log("Message sent: %s", info.response);
+  });
 }
 
 export async function getMailServer() {
-    let mailServer = await vscode.window.showInputBox({
-        placeHolder: "host:port",
-        prompt: "Set mail server (host:port)",
-    });
+  let mailServer = await vscode.window.showInputBox({
+    placeHolder: "host:port",
+    prompt: "Set mail server (host:port)",
+  });
 
-    mailServer = mailServer || vscode.workspace.getConfiguration("mdm").get("mailServer");
+  mailServer =
+    mailServer || vscode.workspace.getConfiguration("mdm").get("mailServer");
 
-    if (mailServer === undefined) {
-        console.log("mailServer is undefined");
-        return "";
-    }
-    return mailServer;
+  if (mailServer === undefined) {
+    console.log("mailServer is undefined");
+    return "";
+  }
+  return mailServer;
 }
