6a4c30106cf7611dc4d9ac2aed76e073adee4105
Changsu Im <imcxsu@gmail.com>
2023-02-20 22:25:49 +0900

    Convert markdown to html
    - Using markdown-it

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,37 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as MarkdownIt from "markdown-it";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    console.log("activate");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "mdm" is now active!');
+    const md: MarkdownIt = new MarkdownIt();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('mdm.send', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Markdown Mail!');
-	});
+    let send = vscode.commands.registerCommand('mdm.send', async () => {
 
-	context.subscriptions.push(disposable);
+        if (!vscode.window.activeTextEditor) {
+            return; // no editor
+        }
+
+        const {document} = vscode.window.activeTextEditor;
+
+        const scheme = document.uri.scheme;
+        if (!scheme || scheme !== "file") {
+            return; // not a file
+        }
+        console.log(document.uri);
+
+        const message = `# current file ${document.uri.path}`;
+        console.log(message);
+
+        const html = md.render(message);
+        console.log(html);
+
+        vscode.window.showInformationMessage('mdm Done!');
+    });
+
+    context.subscriptions.push(send);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+    console.log("deactivate");
+}
