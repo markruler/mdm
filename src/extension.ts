1c0e0fa8e50c084ddfb982067caf5fd06515152d
Changsu Im <imcxsu@gmail.com>
2023-02-22 00:19:08 +0900

    Extract function sendMarkdown

--- src/extension.ts
+++ src/extension.ts
@@ -12,12 +12,9 @@ export interface UserOption {
 }
 
 /**
- * Activate extension
- * @param context Extension context
+ * Send markdown to mail server
  */
-export async function activate(context: vscode.ExtensionContext) {
-  console.log("Activate MDM");
-
+async function sendMarkdown() {
   const markdown: MarkdownIt = new MarkdownIt().use(
     // Remove front matter
     require("markdown-it-front-matter"),
@@ -26,41 +23,48 @@ export async function activate(context: vscode.ExtensionContext) {
     }
   );
 
-  let send = vscode.commands.registerCommand("mdm.send", async () => {
-    if (!vscode.window.activeTextEditor) {
-      return; // no editor
-    }
+  if (!vscode.window.activeTextEditor) {
+    return; // no editor
+  }
 
-    const { document } = vscode.window.activeTextEditor;
+  const { document } = vscode.window.activeTextEditor;
 
-    const scheme = document.uri.scheme;
-    if (!scheme || scheme !== "file") {
-      return; // not a file
-    }
-    console.log(`current file ${document.uri}`);
+  const scheme = document.uri.scheme;
+  if (!scheme || scheme !== "file") {
+    return; // not a file
+  }
+  console.log(`current file ${document.uri}`);
 
-    const originalText: string = (await read(document.uri)) || "";
-    const userOption: UserOption = await readMailOptions(originalText);
-    console.log("User Option", userOption);
+  const originalText: string = (await read(document.uri)) || "";
+  const userOption: UserOption = await readMailOptions(originalText);
+  console.log("User Option", userOption);
 
-    if (!userOption.host || !userOption.port) {
-      const mailServer: string = await getMailServer();
-      console.log(`mailServer: ${mailServer}`);
+  if (!userOption.host || !userOption.port) {
+    const mailServer: string = await getMailServer();
+    console.log(`mailServer: ${mailServer}`);
 
-      const mailHostPort = mailServer.split(":");
-      userOption.host = mailHostPort[0];
-      userOption.port = parseInt(mailHostPort[1]);
-    }
+    const mailHostPort = mailServer.split(":");
+    userOption.host = mailHostPort[0];
+    userOption.port = parseInt(mailHostPort[1]);
+  }
 
-    const html = markdown.render(originalText);
+  const html = markdown.render(originalText);
 
-    await sendMail(html, userOption);
+  await sendMail(html, userOption);
 
-    vscode.window.showInformationMessage(
-      `Send to ${userOption.host}:${userOption.port}`
-    );
-  });
+  vscode.window.showInformationMessage(
+    `Send to ${userOption.host}:${userOption.port}`
+  );
+}
 
+/**
+ * Activate extension
+ * @param context Extension context
+ */
+export async function activate(context: vscode.ExtensionContext) {
+  console.log("Activate MDM");
+
+  let send = vscode.commands.registerCommand("mdm.send", () => sendMarkdown());
   context.subscriptions.push(send);
 }
 
